b89a35433f42dd7b50f95a27144e0ad922f20d2d
TeunSpithoven <teun.spithoven@gmail.com>
2024-01-08 16:36:23 +0100

    update test for new success message

--- tests/create-product.spec.ts
+++ tests/create-product.spec.ts
@@ -1,11 +1,9 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('create product', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.getByTestId('product-name-input').fill('haha');
-  await page.getByRole('button', { name: 'Submit' }).click();
-  
-  await expect(page.getByTestId('create-product-output')).toBeVisible();
-  await page.getByTestId('create-product-output').selectText();
+test("create product", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await page.getByTestId("product-name-input").fill("haha");
+  await page.getByRole("button", { name: "Submit" }).click();
+
+  await expect(page.getByText("product created successfully")).toBeAttached();
 });
-
